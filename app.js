15a9aec8bd890fd65e1ad75c88bca584fcd683b3
uidd <boss63081@gmail.com>
2021-06-28 16:00:56 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const exhbs = require('express-handlebars');
+const multer = require('multer');
+//const userdb = require('./lib/userdb.js');
+const AccessSheetData = require('./lib/databaseAPI.js');
+const dbAPI = new AccessSheetData().sheet;
+require('dotenv').config()
+
+const app = express();
+
+app.engine('.hbs',exhbs({
+    defaultLayout:'main',
+    extname:'.hbs',
+    layoutsDir:__dirname+'/views/layouts/',
+    partialsDir:__dirname+'/views/partials/'
+}));
+//set the view engine is .hbs
+app.set('view engine', '.hbs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/public/upload/')      //you tell where to upload the files,
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+const upload = multer({storage: storage});
+
+
+
+app.get('/',(req, res)=>{
+    dbAPI.listsheet('suitAPP', (reply)=>{
+        res.render('home',{
+            sheetList: reply
+        });
+    });
+    // userdb.ListSheet('suitAPP', (reply)=>{
+    //     res.render('home',{
+    //         sheetList: reply
+    //     });
+    // });
+});
+app.get('/listData', (req, res)=>{
+    new dbAPI(req.query.sheetName).listdata('suitAPP', (reply)=>{
+        if(typeof reply !== 'undefined'){
+            res.status(200).send({
+                SheetData: reply
+            });
+        }
+    });
+    // userdb.ListData('suitAPP', req.query.sheetName, (reply)=>{
+    //     if(typeof reply !== 'undefined'){
+    //         res.status(200).send({
+    //             SheetData: reply
+    //         });
+    //     }
+    // });
+});
+app.post('/', upload.single('file'), (req, res)=>{
+    const filetype = req.file['filename'].split(".")[1];
+    dbAPI.importData(req.file.path, filetype,'suitAPP',(err, reply)=>{
+        if(err) console.log(err);
+        if(typeof reply !== 'undefined'){
+            console.log(reply);
+            res.status(200).send({
+                newSheet: reply
+            });
+        }
+        else{
+            console.log("上傳失敗");
+        }
+    });
+    // userdb.ImportExcel(req.file.path,'suitAPP',(err, reply)=>{
+    //     if(err) console.log(err);
+    //     if(typeof reply !== 'undefined'){
+    //         console.log(reply);
+    //         res.status(200).send({
+    //             newSheet: reply
+    //         });
+    //     }
+    //     else{
+    //         console.log("上傳失敗");
+    //     }
+    // });
+});
+app.post('/insertData', (req, res)=>{
+    let inputData = JSON.parse(JSON.stringify(req.body));
+    console.log(inputData);
+    delete inputData.sheetName;
+    new dbAPI(req.body.sheetName).insertdata('suitAPP', inputData,(reply)=>{
+        res.status(200).send({
+            currRow: reply
+        });
+    });
+    // userdb.InsertData('suitAPP',req.body.sheetName, inputData,(reply)=>{
+    //     res.status(200).send({
+    //         currRow: reply
+    //     });
+    // });
+});
+app.listen(3000,()=>{
+    console.log("web build");
+});
---
+++ public/js/style.js
@@ -0,0 +1,125 @@
+$(document).ready(()=>{
+    let file;
+    let currColumnName;
+    // list all data in this selected data and display in the table.
+    $('select').change((e)=>{
+        console.log("change");
+        $('option[value=initial]').attr("disabled", "disabled");
+        $('#append').attr("disabled", false);
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            data: {
+                sheetName: $('option:selected').val()
+            },
+            url: "/listData",
+            success:(data)=>{
+                console.log("success");
+                $(".table").find('thead').empty();
+                const columns = Object.keys(data.SheetData[0]);
+                currColumnName = [...columns];
+                let tableContent = [$('<tr>'),$('<th scope="col">#</th>')];
+                columns.map((e)=>{
+                    tableContent.push($('<th scope="col">' +e+'</th>'));
+                });
+                tableContent.push($('</tr>'));
+                if(data.SheetData[0][columns[0]]==""){
+                    console.log("no data");
+                }
+                else{
+                    data.SheetData.map((e, idx)=>{
+                        tableContent.push($('<tr>'));
+                        tableContent.push($('<th scope="row">' +(idx+1).toString()+'</th>'));
+                        for(let key in columns){
+                            tableContent.push($('<th scope="row">' +e[columns[key]]+'</th>'));
+                        }
+                        tableContent.push($('</tr>'));
+                    });
+                }
+                $(".table").find('thead').append(tableContent);
+            },
+            error: ()=>{
+                console.log("error");
+            }
+        });
+    });
+    // Insert data for the selected sheet.
+    $('#append').click(()=>{
+        // First, make a form fitting the colomns of the selected sheet.
+        $("form").empty();
+        let formContent = new Array();
+        formContent.push($('<p style="text-align: center;font-size: 35px;color: white;">新增資料</p>'));
+        currColumnName.map((e, idx)=>{
+            formContent.push($('<div class="form-group" style="width: 50%;margin-left: 20px; margin:auto"><label>'+e+'</label><input type="text" class="form-control" name="'+String.fromCharCode(65+idx)+'" placeholder="Enter '+e+'">'));
+        })
+        formContent.push($('<p></p>'));
+        formContent.push($('<button type="submit" class="btn btn-primary">Submit</button>'));
+        $("form").append(formContent);
+        
+        // Second, bpopup this form.
+        const bpopup = $('form').bPopup({
+            fadeSpeed: 'slow', 
+            followSpeed: 1500
+        });
+
+        // Third, submit and dynamically update the content of table.
+        $('button[type=submit]').click((event)=>{
+            console.log("click");
+            event.preventDefault();
+            let sendData = {
+                sheetName: $('option:selected').val()
+            };
+            sendData = Object.assign(sendData, $("form").serializeObject());
+            console.log($("form").serializeObject());
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                data: sendData,
+                url: "/insertData",
+                success:(data)=>{
+                    console.log('success');
+                    delete sendData.sheetName;
+                    let tableContent = [$('<tr>'),$('<th scope="row">' +(data.currRow).toString()+'</th>')];
+                    Object.keys(sendData).map((e)=>{
+                        tableContent.push($('<th scope="row">' +sendData[e]+'</th>'));
+                    })
+                    $(".table").find('thead').append(tableContent);
+                    bpopup.close();
+                },
+                error:()=>{
+                    console.log("error");
+                }
+            })
+        });
+    });
+    // If there is a file in upload buffer, enable upload button.
+    $('input[type=file]').change((e)=>{
+        file = e.target.files[0];
+        $('#upload').attr("disabled", false);
+    });
+    // Upload file and update the <option> list.
+    $('#upload').click(()=>{
+        const formdata = new FormData();
+        formdata.append('file', file);
+        $.ajax({
+            type: "POST",
+            processData: false,
+            contentType: false,
+            dataType: "json",
+            data: formdata,
+            url: "/",
+            success:(data)=>{
+                console.log("success");
+                alert("上傳成功");
+                $('input[type=file]').val("");
+                let option_type = data.newSheet.map((e)=>{
+                    return $("<option value='"+e+"'>"+e+"</option>");
+                });
+                $("select").append(option_type);
+            },
+            error: ()=>{
+                console.log("error");
+            }
+        });
+    })
+});
